5ba5cad514c71400f56a34a30048b20efadcb201
Vivian Lu <vivianlu512@gmail.com>
2023-05-02 23:58:58 +0800

    新增

---
+++ vite-project/src/components/CenterBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="centerBar">
+    <div class="centerUp"></div>
+    <div class="centerDown">
+      <button class="dyson">
+        <a href="#">
+          <p class="btnTitle">風扇選物輯</p>
+          <p class="btnDesc">最高回饋20%</p>
+        </a>
+      </button>
+      <button class="twelvePercentOff">
+        <a href="#">
+          <p class="btnTitle">百大品牌</p>
+          <p class="btnDesc">最高回饋</p>
+        </a>
+      </button>
+      <button class="seasonChange">
+        <a href="#">
+          <p class="btnTitle">換季有對策</p>
+          <p class="btnDesc">抗敏收納有一套</p>
+        </a>
+      </button>
+    </div>
+  </div>
+</template>
+<style scoped>
+.centerDown > button {
+  position: relative;
+  margin: 10px;
+  width: 250px;
+  height: 90px;
+  border: none;
+  border-radius: 10px;
+  background-size: 100%;
+  cursor: pointer;
+}
+.dyson {
+  background: url(../assets/PIC/dyson.jpg) no-repeat;
+}
+.twelvePercentOff {
+  background: url(../assets/PIC/12off.jpg) no-repeat;
+}
+.seasonChange {
+  background: url(../assets/PIC/seasonchange.jpg) no-repeat;
+}
+.centerDown a {
+  text-decoration: none;
+  color: white;
+}
+.btnTitle {
+  position: absolute;
+  top: -10px;
+  left: 15px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.btnDesc {
+  position: absolute;
+  top: 20px;
+  left: 15px;
+  font-size: 14px;
+}
+</style>
---
+++ vite-project/src/components/LeftBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="leftbar">
+    <div class="leftUp">
+      <button class="btn">
+        <a href="#">
+          <p class="btnTitle">全場32折起</p>
+          <p class="btnDesc">連假好禮雙重抽</p>
+        </a>
+      </button>
+    </div>
+    <div class="leftDown">
+      <button class="btn">
+        <a href="">
+          <p class="btnTitle">Apple Watch</p>
+          <p class="btnDesc">指定款$119900up</p>
+        </a>
+      </button>
+    </div>
+  </div>
+</template>
+<style scoped>
+.btn {
+  position: relative;
+  width: 200px;
+  height: 210px;
+  font-size: 14px;
+  border: none;
+  border-radius: 10px;
+  margin: 10px 20px;
+  box-shadow: 0 5px #dddddd;
+}
+
+.leftUp > .btn {
+  background: url(../assets/PIC/mother.jpg);
+  background-size: 100%;
+}
+.leftDown > .btn {
+  background: url(../assets/PIC/applewatch.jpg);
+  background-size: 100%;
+}
+
+.btnImg {
+  border-radius: 10px;
+}
+
+.btnTitle {
+  position: absolute;
+  font-weight: bold;
+  font-size: 18px;
+  left: 35%;
+  top: 65%;
+}
+.btnDesc {
+  position: absolute;
+  font-size: 14px;
+  left: 35%;
+  top: 80%;
+}
+.btn > a {
+  text-decoration: none;
+  color: #000;
+}
+.btn:hover > a {
+  color: #0090eb;
+}
+</style>
