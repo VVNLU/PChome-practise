12011a630679ad619418c322cbcdadfcad1bef54
Vivian Lu <vivianlu512@gmail.com>
2023-05-04 19:21:44 +0800

    修改點擊照片超連結

--- vite-project/src/components/CenterBar.vue
+++ vite-project/src/components/CenterBar.vue
@@ -2,62 +2,40 @@
   <div class="centerBar">
     <div class="centerUp"></div>
     <div class="centerDown">
-      <button class="dyson">
-        <a href="#">
-          <p class="btnTitle">風扇選物輯</p>
-          <p class="btnDesc">最高回饋20%</p>
+      <button>
+        <a href="#"
+          ><img src="../assets/PIC/47off.jpg" alt="" class="dyson" />
         </a>
       </button>
-      <button class="twelvePercentOff">
-        <a href="#">
-          <p class="btnTitle">百大品牌</p>
-          <p class="btnDesc">最高回饋</p>
-        </a>
+      <button>
+        <a href="#"><img src="../assets/PIC/esoncreditcard.jpg" alt="" /> </a>
       </button>
-      <button class="seasonChange">
-        <a href="#">
-          <p class="btnTitle">換季有對策</p>
-          <p class="btnDesc">抗敏收納有一套</p>
+      <button>
+        <a href="#"
+          ><img
+            src="../assets/PIC/mother5200.jpg"
+            alt=""
+            class="seasonChange"
+          />
         </a>
       </button>
     </div>
   </div>
 </template>
 <style scoped>
-.centerDown > button {
+img {
   position: relative;
-  margin: 10px;
-  width: 250px;
-  height: 90px;
-  border: none;
   border-radius: 10px;
-  background-size: 100%;
-  cursor: pointer;
+  width: 250px;
+  height: 100px;
+  left: 230px;
+  top: -110px;
 }
-.dyson {
-  background: url(../assets/PIC/dyson.jpg) no-repeat;
+button {
+  border: none;
+  margin: 5px;
 }
-.twelvePercentOff {
-  background: url(../assets/PIC/12off.jpg) no-repeat;
-}
-.seasonChange {
-  background: url(../assets/PIC/seasonchange.jpg) no-repeat;
-}
-.centerDown a {
+a {
   text-decoration: none;
-  color: white;
-}
-.btnTitle {
-  position: absolute;
-  top: -10px;
-  left: 15px;
-  font-size: 18px;
-  font-weight: bold;
-}
-.btnDesc {
-  position: absolute;
-  top: 20px;
-  left: 15px;
-  font-size: 14px;
 }
 </style>
